687e32e90325d2948f09eb5765a02f0cb0dd1f19
teddyrobin30-stack <teddyrobin30@gmail.com>
2026-02-07 21:30:42 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ import {
 
 // --- COMPONENTS ---
 import DashboardComponent from './components/MainDashboard'; 
-import AgendaView from './components/AgendaView.tsx';
+import AgendaView from './components/AgendaView';
 import ContactsView from './components/ContactsView';
 import TasksView from './components/TasksView';
 import GroupsView from './components/GroupsView';
